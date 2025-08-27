49e7b984dbfd0d5f3cc926440ac447bfa67d1641
Giuseppe Mammolo <gmammolo@gmail.com>
2025-08-27 11:02:33 +0200

    example class

---
+++ CompfyuiGmammolo.py
@@ -0,0 +1,42 @@
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+import os
+
+class RemoveThinkPrompt:
+    def __init__(self):
+        self._base_dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "saved_context"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                    {"context": ("STRING", {"forceInput": True},),
+                     "filename": ("STRING", {"default": "context"})},
+                }
+
+    RETURN_TYPES = ()
+    FUNCTION = "ollama_save_context"
+
+    OUTPUT_NODE = True
+    CATEGORY = "Ollama"
+
+    def ollama_save_context(self, filename, context=None):
+        path = self._base_dir + os.path.sep + filename
+        metadata = PngInfo()
+
+        metadata.add_text("context", ','.join(map(str, context)))
+
+        image = Image.new('RGB', (100, 100), (255, 255, 255))  # Creates a 100x100 white image
+
+        image.save(path + ".png", pnginfo=metadata)
+
+        return {"ui": {"context": context}}
+
+
+
+NODE_CLASS_MAPPINGS = {
+    "RemoveThinkPrompt": RemoveThinkPrompt,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "RemoveThinkPrompt": "Remove Think Prompt",
+}
